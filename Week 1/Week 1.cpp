a4a9d146cec350521c7deb3a717aa4af9c0e1232
tranq <tranq@DESKTOP-0074J4N>
2022-09-07 05:54:58 -0400

    Add project files.

---
+++ Week 1/Week 1.cpp
@@ -0,0 +1,23 @@
+﻿// Week 1.cpp : Defines the entry point for the application.
+//
+
+#include "Week 1.h"
+#pragma message("Hello world")
+
+using namespace std;
+
+#define name "Eric"
+#define age 40
+
+int main()
+{
+#if age<40
+	cout << "You are under 40" << endl;
+#elif age < 60
+	cout << "You are under 60" << endl;
+#else
+	cout << "You are over 60" << endl;
+#endif
+
+	return 0;
+}
---
+++ Week 1/Week 1.h
@@ -0,0 +1,12 @@
+﻿// Week 1.h : Include file for standard system include files,
+// or project specific include files.
+
+#ifndef Week1_h
+#define Week1_h
+#endif
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
