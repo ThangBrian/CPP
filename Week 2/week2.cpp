dec12063242493878e31a3dbbf8353fc3732f64d
QuocThangTran <tran0443@algonquinlive.com>
2022-09-16 09:10:01 -0400

    initial commit

--- Week 2/week2.cpp
+++ Week 2/week2.cpp
@@ -1,39 +1,8 @@
 
 #include<iostream>
-using namespace std;
-namespace cst8219 {
-	class MyFirstClass{ };
-}
-class MyFirstClass {
-private:
-	int age;
-	char* firstName, * lastName;
-public:
-	MyFirstClass() : MyFirstClass(0, NULL, NULL) { 
-		int i = 0; i++;
-	}
-	MyFirstClass(int a, char* f, char* l) {
-		age = a;
-		firstName = f;
-		lastName = l;
-	}
 
-	~MyFirstClass() {
-		int i = 0; i++;
-	}
-
-	int getAge() { return age; }
-	char* getFirstName() { return firstName; }
-	char* getLastName() { return lastName; }
-
-protected:
-	void setFirstName(char* f) { firstName = f; }
-	void setLastName(char* l) { lastName = l;  }
-};
-class MySecondClass { };
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-	MyFirstClass mfc;
-   cout << "Hello World!" << endl;
-   return 0;
+	std::cout << "Hello World!" << std::endl;
+	return 0;
 }
