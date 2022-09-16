d9b66a533da6216aa919bfd23a36f626cf736a26
QuocThangTran <tran0443@algonquinlive.com>
2022-09-16 09:21:12 -0400

    Finished constructors

--- Week 2/week2.cpp
+++ Week 2/week2.cpp
@@ -1,8 +1,25 @@
 
 #include<iostream>
 
+using namespace std;
+
+namespace CST8219 {
+	class Vehicle {
+	private:
+		int numWheels;
+		int	numDoors;
+	public:
+		Vehicle(int w, int d) {
+			numWheels = w;
+			numDoors = d;
+		}
+		Vehicle(int w) : Vehicle(w, 4) {}
+		Vehicle() : Vehicle(4) {}
+	};
+}
+
 int main(int argc, char** argv)
 {
-	std::cout << "Hello World!" << std::endl;
+	cout << "Hello World!" << endl;
 	return 0;
 }
