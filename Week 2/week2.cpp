8817a274f0d42b6cd1bdf2bbdb204cc661c2be6e
QuocThangTran <tran0443@algonquinlive.com>
2022-09-16 09:23:50 -0400

    Finished destructor

--- Week 2/week2.cpp
+++ Week 2/week2.cpp
@@ -12,9 +12,17 @@ namespace CST8219 {
 		Vehicle(int w, int d) {
 			numWheels = w;
 			numDoors = d;
+			cout << "In constructor with 2 parameters" << endl;
 		}
-		Vehicle(int w) : Vehicle(w, 4) {}
-		Vehicle() : Vehicle(4) {}
+		Vehicle(int w) : Vehicle(w, 4) {
+			cout << "In constructor with 1 parameters, wheels = " << w  << endl;
+		}
+		Vehicle() : Vehicle(4) {
+			cout << "In constructor with 0 parameters" << endl;
+		}
+		~Vehicle() {
+			cout << "In destructor" << endl;
+		}
 	};
 }
 
