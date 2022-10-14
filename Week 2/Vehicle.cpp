9ce33e20f5e33ca33f3aed8dc37bfca9ca38f9b2
QuocThangTran <tran0443@algonquinlive.com>
2022-10-13 23:47:46 -0400

    Finished Week 5

--- Week 2/Vehicle.cpp
+++ Week 2/Vehicle.cpp
@@ -1,4 +1,5 @@
 #include"Vehicle.h"
+#include <string>
 #include <iostream>
 
 
@@ -6,21 +7,69 @@ using namespace std;
 Vehicle::Vehicle(int w, int d) {
 	numWheels = w;
 	numDoors = d;
-	cout << "In constructor with 2 parameters" << endl;
+	//cout << "In constructor with 2 parameters" << endl;
 }
 Vehicle::Vehicle() : Vehicle(4, 2) {
-	cout << "In constructor with 0 parameters" << endl;
+	//cout << "In constructor with 0 parameters" << endl;
 }
 Vehicle::~Vehicle() {
-	cout << "In destructor" << endl;
+	//cout << "In destructor" << endl;
 }
 
 Vehicle::Vehicle(Vehicle& copy):Vehicle(copy.getNumWheels(), copy.getNumDoors()) {
-	cout << "In copy constructor by reference" << endl;
+	//cout << "In copy constructor by reference" << endl;
 }
 
 Vehicle::Vehicle(Vehicle* copy) : Vehicle(*copy) { 
-	cout << "In copy constructor by pointer" << endl;
+	//cout << "In copy constructor by pointer" << endl;
+}
+
+Vehicle& Vehicle::operator=(Vehicle& const V) {
+	this->setNumWheels(V.getNumWheels());
+	this->setNumDoors(V.getNumDoors());
+	return *this;
+}
+
+bool Vehicle::operator==(Vehicle& const V) {
+	if (this->getNumWheels() == V.getNumWheels() && this->getNumDoors() == V.getNumDoors())
+		return true;
+	return false;
+
+}
+
+bool Vehicle::operator!=(Vehicle& const V) {
+	return !operator==(V);
+}
+
+Vehicle Vehicle::operator++(int i) { //post-fix
+	Vehicle temp = *this;
+	operator++();
+	return temp;
+}
+
+Vehicle Vehicle::operator++() {
+	this->setNumWheels(this->getNumWheels() + 1);
+	this->setNumDoors(this->getNumDoors() + 1);
+	return *this;
+}
+
+Vehicle Vehicle::operator--(int i) { // post-fix
+	Vehicle temp = *this;
+	operator--();
+	return temp;
+}
+
+Vehicle Vehicle::operator--() {
+	this->setNumWheels(this->getNumWheels() - 1);
+	this->setNumDoors(this->getNumDoors() - 1);
+	return *this;
+}
+
+ostream& operator<<(ostream& out, const Vehicle& V)
+{
+	out << "The number of wheels is: " << V.numWheels
+	<< " The number of doors is : " << V.numDoors;
+	return out;
 }
 
 void Vehicle::setNumWheels(int w) { numWheels = w; }
--- Week 2/Vehicle.h
+++ Week 2/Vehicle.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include <iostream>
+using namespace std;
 class Vehicle {
 private:
 	int numWheels;
@@ -15,4 +16,12 @@ public:
 	int  getNumWheels(void);
 	void setNumDoors(int);
 	int  getNumDoors(void);
+	Vehicle& operator=(Vehicle& V);
+	bool operator==(Vehicle& V);
+	bool operator!=(Vehicle& V);
+	Vehicle operator++();
+	Vehicle operator++(int) ;
+	Vehicle operator--();
+	Vehicle operator--(int);
+	friend ostream& operator<<(ostream& os, const Vehicle& V);
 };
